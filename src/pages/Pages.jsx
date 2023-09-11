48af3b4bb10d1c4b91b7e1bbb6d72eeea0ccd557
Martins Oloyede <oloyedemartins15@gmail.com>
2023-09-12 00:02:20 +0100

    added lazy loading

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,26 +1,27 @@
-import React from 'react'
-import Home from './Home'
-import Cuisine from './Cuisine'
-import Recipe from './Recipe';
-import Searched from './Searched';
+import React, { lazy, Suspense } from "react";
+import Searched from "./Searched";
 import { Route, Routes, useLocation } from "react-router-dom";
-import { AnimatePresence } from 'framer-motion';
+import { AnimatePresence } from "framer-motion";
 
+const Home = lazy(() => import("./Home"));
+const Cuisine = lazy(() => import("./Cuisine"));
+const Recipe = lazy(() => import("./Recipe"));
 
 const Pages = () => {
   const location = useLocation();
 
   return (
-    <AnimatePresence mode='wait'>
-      <Routes location={location} key={location.path}>
-        <Route path="/" element={<Home />} />
-        <Route path="/cuisine/:type" element={<Cuisine />} />
-        <Route path="/searched/:search" element={<Searched />} />
-        <Route path="/recipe/:name" element={<Recipe />} />
-       </Routes>
+    <AnimatePresence mode="wait">
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Routes location={location} key={location.path}>
+          <Route path="/" element={<Home />} />
+          <Route path="/cuisine/:type" element={<Cuisine />} />
+          <Route path="/searched/:search" element={<Searched />} />
+          <Route path="/recipe/:name" element={<Recipe />} />
+        </Routes>
+      </Suspense>
     </AnimatePresence>
-       
-  )
-}
+  );
+};
 
-export default Pages
+export default Pages;
